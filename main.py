d5822b701c6a68b966e34197d3dc91efb527b02c
T1NG3R <bohutenko@gmail.com>
2024-02-26 01:07:54 +0200

    Added progress bar

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from pytube import Playlist
-from moviepy.video.io import ffmpeg_tools
 import os
+
+from moviepy.video.io import ffmpeg_tools
+from pytube import Playlist, YouTube
+from pytube.cli import on_progress
 
 playlist_url = input('Enter playlist url:\n')
 playlist = Playlist(playlist_url)
 
-for video in playlist.videos:
-    video_streams = video.streams.filter()
+for video in playlist.video_urls:
+    yt = YouTube(video, on_progress_callback=on_progress)
+    video_streams = yt.streams.filter()
     best_video = sorted(video_streams, key=lambda x: int(x.resolution[:-1]) if x.resolution else 0, reverse=True)[0]
-    print(f"«{video.title}» downloading.")
-    video_filename = f"{video.title}.mp4"
+    print(f"«Downloading {yt.title}».")
+    video_filename = f"{yt.title}.mp4"
     best_video.download(filename=video_filename)
-    print(f"«{video.title}» downloaded.")
+    print("")
     if not best_video.includes_audio_track:
-        print(f"«{video.title}» has no audio track, downloading")
-        audio_filename = f"{video.title}.mp3"
-        video.streams.get_audio_only().download(filename=audio_filename)
-        print(f"Audio for «{video.title}» downloaded as «{audio_filename}»")
-        final_video = f"final {video_filename}"
-        ffmpeg_tools.ffmpeg_merge_video_audio(video_filename, audio_filename, final_video)
+        print(f"«{yt.title}» has no audio track, downloading")
+        audio_filename = f"{yt.title}.mp3"
+        yt.streams.get_audio_only().download(filename=audio_filename)
+        print(f"Audio for «{yt.title}» downloaded as «{audio_filename}»")
+        merged_video = f"merged {video_filename}"
+        ffmpeg_tools.ffmpeg_merge_video_audio(video_filename, audio_filename, merged_video, logger=None)
         os.remove(video_filename)
-        os.rename(final_video, video_filename)
+        os.rename(merged_video, video_filename)
         os.remove(audio_filename)
