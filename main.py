4cce5b9fb3ba771df5b51bea869b59b0dcd09759
T1NG3R <bohutenko@gmail.com>
2024-02-26 02:45:48 +0200

    Added support for both video and playlist URLs

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from moviepy.video.io import ffmpeg_tools
 from pytube import Playlist, YouTube
 from pytube.cli import on_progress
 
-playlist_url = input('Enter playlist url:\n')
-playlist = Playlist(playlist_url)
 
-for video in playlist.video_urls:
+def download_video(video):
     yt = YouTube(video, on_progress_callback=on_progress)
     video_streams = yt.streams.filter()
     best_video = sorted(video_streams, key=lambda x: int(x.resolution[:-1]) if x.resolution else 0, reverse=True)[0]
@@ -19,9 +17,26 @@ for video in playlist.video_urls:
         print(f"«{yt.title}» has no audio track, downloading")
         audio_filename = f"{yt.title}.mp3"
         yt.streams.get_audio_only().download(filename=audio_filename)
-        print(f"Audio for «{yt.title}» downloaded as «{audio_filename}»")
+        print("")
+        print(f"Audio for «{yt.title}» downloaded, merging video and audio")
         merged_video = f"merged {video_filename}"
         ffmpeg_tools.ffmpeg_merge_video_audio(video_filename, audio_filename, merged_video, logger=None)
         os.remove(video_filename)
         os.rename(merged_video, video_filename)
         os.remove(audio_filename)
+        print(f"{video_filename} merged successfully")
+
+
+def main():
+    url = input('Enter url:\n')
+    if 'playlist?list=' in url.lower():
+        print("Entered playlist url, downloading videos:\n")
+        playlist = Playlist(url)
+        for video_url in playlist.video_urls:
+            download_video(video_url)
+    else:
+        download_video(url)
+
+
+if __name__ == "__main__":
+    main()
